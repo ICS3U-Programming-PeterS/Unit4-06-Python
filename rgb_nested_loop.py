cd8d2bbdb7bb811d100b7dc77dc5e4bcf59e6d97
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-11-21 09:58:15 +0000

    Added rgb_nested_loop.py

--- rgb_nested_loop.py
+++ rgb_nested_loop.py
@@ -2,21 +2,23 @@
 
 # Created By: Peter Sobowale
 # Date: November 16, 2022
-# This program prints every possible RGB color 
+# This program prints every possible RGB color
+
 
 def main():
-    # initialize variables 
+    # initialize variables
     red = 0
     green = 0
     blue = 0
 
-    # for loop tp print every color possibility
+    # for loop to print every color possibility
     for red in range(256):
         print(f"\033[38;2;{red};{green};{blue}mRGB ({red},{green},{blue})")
         for green in range(256):
             print(f"\033[38;2;{red};{green};{blue}mRGB ({red},{green},{blue})")
-            for blue in range (256):
+            for blue in range(256):
                 print(f"\033[38;2;{red};{green};{blue}mRGB ({red},{green},{blue})")
+
 
 if __name__ == "__main__":
     main()
